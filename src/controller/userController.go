914b5c7f5582084c10044d27efa899c2fca5e1d7
diovanavalim <diovana.valim@mercadolivre.com>
2022-08-30 13:54:04 -0300

    feat: use user repository

--- src/controller/userController.go
+++ src/controller/userController.go
@@ -1,9 +1,46 @@
 package controller
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/model"
+	"api/src/repository"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Creating New User!"))
+	w.Header().Set("Content-Type", "application/json")
+
+	requestBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user model.User
+
+	if err = json.Unmarshal(requestBody, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userRepository := repository.CreateUserRepository(db)
+
+	result, err := userRepository.CreateUser(user)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("User successfully inserted, ID: %d", result)))
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
