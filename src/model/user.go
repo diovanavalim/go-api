e9a7c14cd121139296b950b479662cbc8d6e4bee
diovanavalim <diovana.valim@mercadolivre.com>
2022-08-30 15:32:30 -0300

    feat: add model validation

--- src/model/user.go
+++ src/model/user.go
@@ -1,6 +1,12 @@
 package model
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+)
 
 type User struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -9,4 +15,24 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+func (user User) Validate() error {
+	v := reflect.ValueOf(user)
+
+	typeOfUser := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		if strings.TrimSpace(fmt.Sprint(v.Field(i).Interface())) == "" {
+			return errors.New(fmt.Sprintf("Field %s can not be empty", typeOfUser.Field(i).Name))
+		}
+	}
+
+	return nil
+}
+
+func (user *User) Format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Email = strings.TrimSpace(user.Email)
+	user.Nickname = strings.TrimSpace(user.Nickname)
 }
