3601f78cd7676c7c91b15fa72c70f992ec417bd4
diovanavalim <diovana.valim@mercadolivre.com>
2022-08-30 15:32:12 -0300

    feat: add package response

---
+++ src/response/response.go
@@ -0,0 +1,26 @@
+package response
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Error(w http.ResponseWriter, statusCode int, err error) {
+	JSON(w, statusCode, struct {
+		Message    string `json:"message"`
+		StatusCode int    `json:"status"`
+	}{
+		Message:    err.Error(),
+		StatusCode: statusCode,
+	})
+}
