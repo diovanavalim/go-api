e1a406c295e0058704585e9408122bee382453d4
diovanavalim <diovana.valim@mercadolivre.com>
2022-09-01 14:39:05 -0300

    feat: add posts table

--- sql/init.sql
+++ sql/init.sql
@@ -2,8 +2,9 @@ CREATE DATABASE IF NOT EXISTS devbook;
 
 USE devbook;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
     id int auto_increment primary key,
@@ -12,7 +13,7 @@ CREATE TABLE users(
     email varchar(50) not null unique,
     password varchar(120) not null,
     createdAt timestamp default current_timestamp()
-) ENGINE=INNODB;
+);
 
 CREATE TABLE followers(
     user_id int not null,
@@ -26,4 +27,18 @@ CREATE TABLE followers(
     ON DELETE CASCADE,
 
     primary key (user_id, follower_id)
-) ENGINE=INNODB;
+);
+
+CREATE TABLE posts(
+    id int auto_increment primary key,
+    title varchar(50) not null,
+    content varchar(300) not null,
+
+    author_id int not null,
+    FOREIGN KEY (author_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+
+    likes int default 0,
+    createdAt timestamp default current_timestamp()
+);
