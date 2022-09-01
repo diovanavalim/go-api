012e224dd82f02bd3dfb617acd6d806f4ebf0f00
diovanavalim <diovana.valim@mercadolivre.com>
2022-09-01 15:35:47 -0300

    feat: add validate fields func

--- src/model/post.go
+++ src/model/post.go
@@ -1,6 +1,12 @@
 package model
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+)
 
 type Post struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -9,4 +15,18 @@ type Post struct {
 	AuthorID  uint64    `json:"authorId,omitempty"`
 	Likes     uint64    `json:"likes"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+func (post Post) Validate(stage string) error {
+	v := reflect.ValueOf(post)
+
+	typeOfPost := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		if strings.TrimSpace(fmt.Sprint(v.Field(i).Interface())) == "" {
+			return errors.New(fmt.Sprintf("Field %s can not be empty", typeOfPost.Field(i).Name))
+		}
+	}
+
+	return nil
 }
